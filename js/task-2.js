17f58d47473cc52fce2c38a14d3385e24c8a3165
OleksijHr <hrymovskij@gmail.com>
2024-02-06 10:01:18 +0200

    changed hw3

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
 function makeArray(firstArray, secondArray, maxLength) {
-    return allAray = firstArray.concat(secondArray).length > maxLength ? firstArray.concat(secondArray).slice(0, maxLength) : firstArray.concat(secondArray);
+    const allAray  = firstArray.concat(secondArray).length > maxLength ? firstArray.concat(secondArray).slice(0, maxLength) : firstArray.concat(secondArray)
+    return allAray;
 }
+
